e782c3db310b8ecb80cc271e11c108e44476394a
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-06-05 05:43:05 +0300

    completed assignment

--- customer.py
+++ customer.py
@@ -1,38 +1,32 @@
-# import sqlite3
-
-# # Connect to the database or create it if it doesn't exist
-# conn = sqlite3.connect('student.db')
-
-# # Create a cursor object to interact with the database
-# cursor = conn.cursor()
-
-# # Create a table to store the data
-# cursor.execute('''CREATE TABLE IF NOT EXISTS users (
-#                     id INTEGER PRIMARY KEY,
-#                     first_name TEXT,
-#                     last_name TEXT,
-#                     email TEXT,
-#                     gender TEXT
-#                 )''')
-
-# # Insert the data into the table
-# cursor.execute('''INSERT INTO users (id, first_name, last_name, email, gender)
-#                   VALUES (1, 'Clem', 'Cowap', 'ccowap0@cloudflare.com', 'Male')''')
-
-# # Commit the changes and close the connection
-# conn.commit()
-# conn.close()
-from sqlalchemy import String, Column, Integer
+from sqlalchemy import String, Column, Integer, create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
+engine = create_engine("sqlite:///db.db")
 Base = declarative_base()
+
 class Customer(Base):
     __tablename__ = 'customer'
-    first_name = Column (String, nullable=False)
-    last_name = Column (String, nullable=False)
-    email = Column (String, nullable=False)
-    gender = Column (String, nullable=False)
+    customer_id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    gender = Column(String, nullable=False)
 
-    engine = create_engine("sqlite:///db.db")
-    Base.metadata.create_all(engine)
-    sessionmaker = sessionmaker(bind=engine)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# from sqlalchemy import String, Column, Integer
+# from sqlalchemy.orm import declarative_base, sessionmaker
+
+# Base = declarative_base()
+# class Customer(Base):
+#     __tablename__ = 'customer'
+#     first_name = Column (String, nullable=False)
+#     last_name = Column (String, nullable=False)
+#     email = Column (String, nullable=False)
+#     gender = Column (String, nullable=False)
+
+#     engine = create_engine("sqlite:///db.db")
+#     Base.metadata.create_all(engine)
+#     sessionmaker = sessionmaker(bind=engine)
--- main.py
+++ main.py
@@ -1,117 +1,165 @@
-# from fastapi import FastAPI
-
-# app = FastAPI()
-
-
-# @app.get("/")
-# def get_root():
-#     return {"message": "Hello, GET request!"}
-
-
-# @app.post("/")
-# def post_root(post_data: str):
-#     return {"message": f"Hello, POST request! You sent: {post_data}"}
-
-
-# @app.put("/")
-# def put_root(put_data: str):
-#     return {"message": f"Hello, PUT request! You sent: {put_data}"}
-
-
-# @app.patch("/")
-# def patch_root(patch_data: str):
-#     return {"message": f"Hello, PATCH request! You sent: {patch_data}"}
-
-
-# @app.delete("/")
-# def delete_root():
-#     return {"message": "Hello, DELETE request!"}
-
-
-# if __name__ == "__main__":
-#     import uvicorn
-
-#     uvicorn.run(app, host="localhost", port=8000)
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List, Optional
-from customer import Customer, sessionmaker
-
-# class PostData(BaseModel):
-#     post_data: str
-
-# class PutData(BaseModel):
-#     put_data: str
-
-# class PatchData(BaseModel):
-#     patch_data: str
-
-
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 app = FastAPI()
 
+engine = create_engine("sqlite:///db.db")
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class Customer(Base):
+    __tablename__ = 'customer'
+    customer_id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    gender = Column(String, nullable=False)
+
 class CustomerSchema(BaseModel):
-    customer_id:int
+    customer_id: int
     first_name: str
     last_name: str
     email: str
     gender: str
 
     class Config:
-        orm_mode=True
+        orm_mode = True
 
 class UpdateCustomerSchema(BaseModel):
     customer_id: Optional[int] = None
-    first_name: Optional [str] = None
-    last_name: Optional [str] = None
-    email: Optional [str] = None
-    gender: Optional [str] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    email: Optional[str] = None
+    gender: Optional[str] = None
 
     class Config:
-        orm_mode=True
+        orm_mode = True
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def get_root():
-    customer = session.query(Customer).all()
+    db = SessionLocal()
+    customers = db.query(Customer).all()
+    db.close()
     return {"message": "Hello, GET request!"}
 
 @app.get("/customer/{id}")
-def get_one_customer(id:int)->CustomerSchema:
-    single_customer = session.query(Customer).filter_by().first()
-    return single_student
-
-# @app.post("/students/")
-# def post_root(post_data: PostData):
-#     return {"message": f"Hello, POST request! You sent: {post_data.post_data}"}
+def get_one_customer(id: int) -> CustomerSchema:
+    db = SessionLocal()
+    single_customer = db.query(Customer).filter_by(customer_id=id).first()
+    db.close()
+    return single_customer
 
 @app.post("/customer/")
-def add_data(customer: CustomerSchema)->CustomerSchema:
-    customer = Customer (**dict(customer))
-    session.add(customer)
-    session.commit()
+def add_data(customer: CustomerSchema) -> CustomerSchema:
+    db = SessionLocal()
+    new_customer = Customer(**dict(customer))
+    db.add(new_customer)
+    db.commit()
+    db.refresh(new_customer)
+    db.close()
 
-    return customer
-
-@app.put("/customer/")
-def put_root(put_data: PutData):
-    return {"message": f"Hello, PUT request! You sent: {put_data.put_data}"}
-
+    return new_customer
 
 @app.patch("/customer/update/{id}")
-def update_customer(id:int, payload:UpdateCustomerSchema )-> CustomerSchema:
-    customer = session.query(customer).filter_by(customer_id = id).first()
-    return {"message": f"Hello, PATCH request! You sent: {patch_data.patch_data}"}
-
+def update_customer(id: int, payload: UpdateCustomerSchema) -> CustomerSchema:
+    db = SessionLocal()
+    customer = db.query(Customer).filter_by(customer_id=id).first()
+    
+    for key, value in dict(payload).items():
+        if value is not None:
+            setattr(customer, key, value)
+    
+    db.commit()
+    db.refresh(customer)
+    db.close()
+    
+    return customer
 
 @app.delete("/customer/delete/{id}")
-def delete_customer(id : int)-> None:
-    customer = session.query(Customer).filter_by(customer-id).first().delete()
-    session.delete(customer)
-    session.commit()
-    return {"message": f"Customer, {id} deleted request"}
+def delete_customer(id: int) -> None:
+    db = SessionLocal()
+    customer = db.query(Customer).filter_by(customer_id=id).first()
+    db.delete(customer)
+    db.commit()
+    db.close()
+    
+    return {"message": f"Customer {id} deleted"}
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
+
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+# from typing import List, Optional
+# from customer import Customer, sessionmaker
+
+# app = FastAPI()
+
+# class CustomerSchema(BaseModel):
+#     customer_id:int
+#     first_name: str
+#     last_name: str
+#     email: str
+#     gender: str
+
+#     class Config:
+#         orm_mode=True
+
+# class UpdateCustomerSchema(BaseModel):
+#     customer_id: Optional[int] = None
+#     first_name: Optional [str] = None
+#     last_name: Optional [str] = None
+#     email: Optional [str] = None
+#     gender: Optional [str] = None
+
+#     class Config:
+#         orm_mode=True
+
+# @app.get("/")
+# def get_root():
+#     customer = session.query(Customer).all()
+#     return {"message": "Hello, GET request!"}
+
+# @app.get("/customer/{id}")
+# def get_one_customer(id:int)->CustomerSchema:
+#     single_customer = session.query(Customer).filter_by().first()
+#     return single_student
+
+# @app.post("/customer/")
+# def add_data(customer: CustomerSchema)->CustomerSchema:
+#     customer = Customer (**dict(customer))
+#     session.add(customer)
+#     session.commit()
+
+#     return customer
+
+# @app.put("/customer/")
+# def put_root(put_data: PutData):
+#     return {"message": f"Hello, PUT request! You sent: {put_data.put_data}"}
 
 
-# if __name__ == "__main__":
-#     import uvicorn
+# @app.patch("/customer/update/{id}")
+# def update_customer(id:int, payload:UpdateCustomerSchema )-> CustomerSchema:
+    
+#     customer = session.query(customer).filter_by(customer_id = id).first()
+#     # breakpoint()
+#     for key, value in dict(payload).items(exclude_unset = True).items():
+#         setattr(customer, key, value)
+#         session.commit()
+#     return customer
 
-#     uvicorn.run(app, host="localhost", port=8000)
+
+# @app.delete("/customer/delete/{id}")
+# def delete_customer(id : int)-> None:
+#     customer = session.query(Customer).filter_by(customer-id).first().delete()
+#     session.delete(customer)
+#     session.commit()
+#     return {"message": f"Customer, {id} deleted request"}
